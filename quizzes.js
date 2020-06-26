6b8444948ff7772531a5bdbc06633a694cd952e4
Beaux Brandon <beauxbrandon11@gmail.com>
2020-06-26 14:37:35 -0700

    first commit

---
+++ quizzes.js
@@ -0,0 +1,39 @@
+const admin = require('firebase-admin');
+const fs = require('fs-extra');
+const yaml = require('yamljs');
+
+admin.initializeApp({
+    credential: admin.credential.cert(require('./credentials.json')),
+});
+const db = admin.firestore();
+
+const quizzes = [
+    'mascots-pro-easy',
+    'mascots-pro-medium',
+    'mascots-pro-hard',
+    'mascots-college-easy',
+    'mascots-college-medium',
+    'mascots-college-hard'
+]
+
+
+const update = async(quizId) => {
+
+    const json = yaml.load(`quizzes/${quizId}.yaml`);
+
+    console.log(JSON.stringify(json));
+
+    const ref = db.collection('quizzes').doc(quizId);
+
+    await ref.set(json, { merge: true });
+
+    console.log('DONE');
+
+}
+
+for (const quiz of quizzes) {
+    update(quiz);
+}
+
+
+
